d3c59c33c7c4e45baa4c71b38b2fb1bdfe893e70
rizmii <rizmiseptian@gmail.com>
2025-10-20 11:42:39 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+let mysql = require("mysql2");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/',(req, res )=>{
+    res.send("Hello World");
+})
+app.listen(PORT,()=>
+{
+    console.log(`server is running on port ${PORT}`);
+})
+const db = mysql.createConnection({
+    host:'localhost',
+    user : 'root',
+    password: '',
+    database: 'biodata',
+    port:3306
+})
