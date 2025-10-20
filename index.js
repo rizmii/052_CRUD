b76f72967e6ac2292f119da74b746f8d31773c83
rizmii <rizmiseptian@gmail.com>
2025-10-20 12:04:17 +0700

    method put

--- index.js
+++ index.js
@@ -21,11 +21,61 @@ const db = mysql.createConnection({
 })
 db.connect((err)=>{
     if(err){
-        console.error('error'+ err.stack);
+        console.error("error"+ err.stack);
         return;
     }
-    console.log('connection success');
+    console.log("connection success");
 
+})
+
+app.get("api/users",(req,res)=>{
+    db.query('SELECT * from users',(err,results)=>{
+        if (err){
+            console.error('error'+ err.stack);
+            res.status(500).send("error fetching users");
+            return;
+
+        }
+        res.json(results);
+    })
+})
+app.post("api/users", (req,res)=>{
+    const {nama,nim,kelas}= req.body;
+
+    if(!nama ||!nim||!kelas){
+        return res.status(400).json({message:"wajib diisi"});
+    }
+    db.query
+    {
+        "INSERT INTO users (nama, nim, kelas) VALUES (?,?,?)",
+        [nama,nim,kelas],
+        (err, results)=>{
+            if(err){
+                console.error(err);
+                return res.status.json({message:"Database error"});
+
+            }
+            res.status(201).json({message:"User created successfully"})
+        }
+    };
+});
+ 
+app.put("api/users/id",(req,res)=>
+{
+    const userId=req.params.id;
+    const{nama,nim,kelas}= req.body;
+    db.query(
+        'UPDATE mahasiswa SET nama=?,nim=?,kelas=?WHERE id=?',
+        [nama,nim,kelas,userId],
+        (err, results)=>{
+            if(err){
+                console.error(err);
+                return res.status(500).json({message: 'database Error'});
+
+            }
+            res.json({message: 'User updated'})
+        }
+    )
 })
 
 
